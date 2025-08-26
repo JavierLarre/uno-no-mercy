cb76182d5c733f7ce58039b9df9a969fa2c9edbb
Javier Larre <javier.larre@uc.cl>
2025-08-25 23:09:54 -0400

    added tests

--- src/scripts/UnoNoMercy.cs
+++ src/scripts/UnoNoMercy.cs
@@ -2,4 +2,8 @@ using Godot;
 
 public partial class UnoNoMercy : Control
 {
+    public override void _Ready()
+    {
+        GD.Print("mish");
+    }
 }
---
+++ tests/Tester.cs
@@ -0,0 +1,13 @@
+using Godot;
+using System;
+using GD_NET_ScOUT;
+
+[Test]
+public partial class Tester : Node
+{
+    [Test]
+    public void AssertTrue()
+    {
+        Assert.IsTrue(true);
+    }
+}
